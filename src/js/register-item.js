7ce20aaff3d465467b8dfc3024e6df2ba1a602f5
Julia Paiva <juliapoliver12@gmail.com>
2024-06-06 10:07:27 -0300

    feat(Add: register-item.js) cleanup(register-product.html)

---
+++ src/js/register-item.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// import { uploadImage } from "./firebase.js"
+
+// Declarando infos do input
+const itemTitle = document.getElementById('item-title')
+const itemDesc = document.getElementById('item-desc')
+const itemSellerName = document.getElementById('item-seller-name')
+const itemSellerTel = document.getElementById('item-seller-tel')
+const itemSellerEmail = document.getElementById('item-seller-email')
+const itemDoc = document.getElementById('item-doc')
+const itemImage = document.getElementById('item-image')
+const itemInicialValue = document.getElementById('item-initial-value')
+const itemReserveValue = document.getElementById('item-reserve-value')
+const itemHistoric = document.getElementById('item-historic')
+const itemPaymentTerm = document.getElementById('item-payment-term')
+const itemReturnTerm = document.getElementById('item-return-term')
+
+// Declarando botão de ação do cadastro
+const buttonItem = document.getElementById('button-register-item') 
