535e8f87e61936bb5db10f5eed9e7e3a0a87f01a
Julia Paiva <123731976+Paivaas@users.noreply.github.com>
2024-06-06 16:47:42 -0300

    feat Criação de  Arquivo(register-item.js) |

--- src/js/register-item.js
+++ src/js/register-item.js
@@ -18,3 +18,40 @@ const itemReturnTerm = document.getElementById('item-return-term')
 
 // Declarando botão de ação do cadastro
 const buttonItem = document.getElementById('button-register-item') 
+
+
+// Funcao que armazena o Toast de sucesso
+const SuccessMessage = () => {
+
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Cadastro concluido com sucesso",
+        showConfirmButton: false,
+        timer: 1500
+    })
+
+}
+
+// Funcao que 
+const SuccessRegister = async() => {
+
+    buttonItem.addEventListener('click', SuccessMessage())
+
+}
+
+SuccessRegister()
+
+
+
+const phoneMask = (value) => {
+    if (!value) return ''
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,'($1) $2')
+    value = value.replace(/(\d)(\d{4})$/,'$1-$2')
+    return value
+}
+
+itemSellerTel.addEventListener('keyup', () => {
+    itemSellerTel.value = phoneMask(itemSellerTel.value)
+})
