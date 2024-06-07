63b18dce4b5ceb5c3be8116bcd1fc8c1fbf623a3
RyanAlvesz <ra546038@gmail.com>
2024-06-06 23:16:30 -0300

    feat: new crud functions

--- src/js/functions.js
+++ src/js/functions.js
@@ -4,10 +4,25 @@ const apiUrl = 'https://leilao-expresso-backend.onrender.com/v1/leilao_expresso'
 
 // #region LOTES
 
-export const getLotes = async() => {
+export const getBatchs = async() => {
 
     try {
         const url = `${apiUrl}/lotes`
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    } catch (error) {
+        return false
+    }
+
+}
+
+// #region USERS
+
+export const postUser = async() => {
+
+    try {
+        const url = `${apiUrl}/usuario`
         const response = await fetch(url)
         const data = await response.json()
         return data
