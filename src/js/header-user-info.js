ad838538980ed429f8b7d360b8cc415077d28c8e
RyanAlvesz <ra546038@gmail.com>
2024-06-11 07:24:49 -0300

    fix: adjusting header icon

--- src/js/header-user-info.js
+++ src/js/header-user-info.js
@@ -4,7 +4,15 @@ const iconImg = document.getElementById('user-icon')
 const userName = document.getElementById('user-name')
 
 const setInfo = () => {
-    iconImg.src = localStorage.getItem('userProfileIcon')
+
+    const userIcon = localStorage.getItem('userProfileIcon')
+
+    if(userIcon != 'null'){
+        iconImg.src = userIcon
+    } else {
+        iconImg.src = 'https://firebasestorage.googleapis.com/v0/b/leilao-expresso.appspot.com/o/profile-icon%2Ficon.png?alt=media&token=2fbadc66-0b13-4eed-8360-7f87ea1076b7'
+    }
+
     iconImg.alt = localStorage.getItem('userName')
     userName.textContent = localStorage.getItem('userName')
 }
